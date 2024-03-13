de6acd3710685fdaa0e5f4dd542f127d90d1fa62
SeoJH27 <morninglibrary@gmail.com>
2024-03-13 17:53:26 +0900

    initial commit

---
+++ app/src/main/java/com/mukitlist/mukitlist/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.mukitlist.mukitlist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mukitlist.mukitlist.ui.theme.MukitlistTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
