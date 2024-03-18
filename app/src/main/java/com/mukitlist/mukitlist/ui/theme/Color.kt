edcc15c262aff382295ea447fea2b78889ed6c9a
SeoJH27 <morninglibrary@gmail.com>
2024-03-18 14:21:45 +0900

    CHORE: Rebase branches

--- app/src/main/java/com/mukitlist/mukitlist/ui/theme/Color.kt
+++ app/src/main/java/com/mukitlist/mukitlist/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package com.mukitlist.mukitlist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/*
+* - 색깔 명칭 방식 -
+*  " nm_theme_light_쓰이는 곳 "
+*
+*   nm: nomal mode
+*       테마가 바뀜에 따라 cm 같은 명칭으로 바뀔 수 있음 (cm: cherry blossom mode)
+*   theme
+*   light
+*       해당 테마의 라이트 모드. Dark 모드는 추후 추가
+*   ex) nm_theme_light_primary
+ */
 
+
+//Light Mode
+val Orange01 = Color(0xFFFE6A38)
+val OrangePink01 = Color(0xFFFC3D08)
+val Pink01 = Color(0xFFFF5454)
+
+
+
+
+
+
+//Dark Mode (아직 지원 미정)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
